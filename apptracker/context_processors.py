d57597f6515199af62a7c8d9c55bdb5ed3088759
Andrew Le <a.asterisk.le@gmail.com>
2024-05-01 13:20:03 -0500

    Added slight statistics to navbar.

--- apptracker/context_processors.py
+++ apptracker/context_processors.py
@@ -1,5 +1,6 @@
 from .models import *
 from datetime import date
+from .utils import *
 
 def get_application_count():
     return Application.objects.count()
@@ -7,8 +8,28 @@ def get_application_count():
 def applied_today():
     return Application.objects.filter(application_date=date.today()).exists()
 
+def get_rate(status_name):
+    total_response_applications = get_response_count()
+    total_status_applications = get_status_application_count(status_name, 1)
+    
+    if total_status_applications == 0:
+        return round(0, 2)
+    
+    if status_name == "Applied":
+        rate = (total_response_applications/total_status_applications) * 100
+    else:
+        rate = (total_status_applications/total_response_applications) * 100
+        
+    return round(rate, 2)
+
 def common_data(request):
     return {
         'applicationcount': get_application_count(),
         'appliedtoday': applied_today(),
+        'responserate': get_rate("Applied"),
+        'rejectionrate': get_rate("Rejected"),
+        'interviewrate': get_rate("Interview"),
+        'withdrawnrate': get_rate("Withdrawn"),
+        'offeredrate': get_rate("Offered"),
+        'acceptedrate': get_rate("Accepted"),
     }
