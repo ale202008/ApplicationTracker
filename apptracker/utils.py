b967ceded94e9572db4f0696ccac268070c4637e
Andrew Le <a.asterisk.le@gmail.com>
2024-04-30 20:34:39 -0500

    Commit 04-30-24

--- apptracker/utils.py
+++ apptracker/utils.py
@@ -1,7 +1,9 @@
 from apptracker.models import *
-import json
 from django.http import JsonResponse
-
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+from datetime import date
+import json
 
 # Functions dedicated to grab applications based on requirements, in decreasing order
 
@@ -22,7 +24,7 @@ def get_all_status_applications(status_name):
 
 # Function to get interview applications based on index in status list
 def get_status_application_count(status_name, index):
-    if status_name == "Rejected" and index == 1:
+    if status_name == "Rejected":
         return len(Application.objects.filter(status=Status.objects.filter(name="Rejected")[index-1]))
     status = Status.objects.filter(name=status_name)
     status_list = Status.objects.filter(status_id__gt = status[index-1].status_id - 1)
@@ -34,6 +36,49 @@ def get_response_count():
     return len(get_all_status_applications("Rejected")) + len(get_all_status_applications("Interview")) + len(get_all_status_applications("Withdrawn"))
 
 # ---- GET FUNCTIONS END --- #
+
+# Function that saves an image
+def save_image(image):
+    if image:
+        fs = FileSystemStorage(location=settings.MEDIA_ROOT)
+        filename =  fs.save(image.name, image)
+        return fs.url(filename)
+    return None
+
+# Function that submits a new application submission
+def application_submission(request):
+    position = request.POST.get('position')
+    pay = request.POST.get('pay')
+    employer_name = request.POST.get('employer') or request.POST.get('other_employer')
+    employer, _ = Employer.objects.get_or_create(name=employer_name) if employer_name else (None, None)
+    location_name = request.POST.get('location') or request.POST.get('other_location')
+    location, _ = Location.objects.get_or_create(name=location_name) if location_name else (None, None)
+    employment_type = request.POST.get('employment_type')
+    source_name = request.POST.get('source') or request.POST.get('other_source')
+    source, _ = Source.objects.get_or_create(name=source_name) if source_name else (None, None)
+    description = request.POST.get('description')
+    notes = request.POST.get('notes')
+    image = save_image(request.FILES.get('image'))
+    application_date = request.POST.get('application_date')
+    applied_status = Status.objects.get(status_id=1, name='Applied')
+    
+    application = Application.objects.create(
+        status=applied_status,
+        application_id = Application.objects.count() + 1,
+        position=position,
+        pay=pay,
+        employer=employer,
+        location=location,
+        employment_type=employment_type,
+        source=source,
+        desc=description,
+        notes=notes,
+        img_url=image,
+        application_date = application_date,
+    )
+    application.save()
+    
+    return JsonResponse({'success': True})
 
 # Function that updates a status based on a drag-drop ui
 def update_status(request):
--- apptracker/views.py
+++ apptracker/views.py
@@ -1,56 +1,15 @@
 from django.shortcuts import render
-from django.core.files.storage import FileSystemStorage
+
 from django.views import View
-from django.conf import settings
+
 from apptracker.models import *
 from .utils import *
-from datetime import date
+
 import os
 
 
 
 # Create your views here.
-
-def save_image(image):
-    if image:
-        fs = FileSystemStorage(location=settings.MEDIA_ROOT)
-        filename =  fs.save(image.name, image)
-        return fs.url(filename)
-    return None
-
-def application_submission(request):
-    position = request.POST.get('position')
-    pay = request.POST.get('pay')
-    employer_name = request.POST.get('employer') or request.POST.get('other_employer')
-    employer, _ = Employer.objects.get_or_create(name=employer_name) if employer_name else (None, None)
-    location_name = request.POST.get('location') or request.POST.get('other_location')
-    location, _ = Location.objects.get_or_create(name=location_name) if location_name else (None, None)
-    employment_type = request.POST.get('employment_type')
-    source_name = request.POST.get('source') or request.POST.get('other_source')
-    source, _ = Source.objects.get_or_create(name=source_name) if source_name else (None, None)
-    description = request.POST.get('description')
-    notes = request.POST.get('notes')
-    image = save_image(request.FILES.get('image'))
-    application_date = request.POST.get('application_date')
-    applied_status = Status.objects.get_or_create(name='Applied')
-    
-    application = Application.objects.create(
-        status=applied_status,
-        application_id = Application.objects.count() + 1,
-        position=position,
-        pay=pay,
-        employer=employer,
-        location=location,
-        employment_type=employment_type,
-        source=source,
-        desc=description,
-        notes=notes,
-        img_url=image,
-        application_date = application_date,
-    )
-    application.save()
-    
-    return JsonResponse({'success': True})
 
 class HomeView(View):
     def get(self, request):
