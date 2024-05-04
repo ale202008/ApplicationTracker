01fabfb44e738fd27d474002c5f8584f7bda0f72
Andrew Le <a.asterisk.le@gmail.com>
2024-05-03 21:35:28 -0500

    Applied 05-03-24

--- apptracker/templatetags/custom_filters.py
+++ apptracker/templatetags/custom_filters.py
@@ -5,6 +5,7 @@ register = template.Library()
 @register.filter
 def remove_https(url):
     url_without_https = url.replace('https://', '')
+    url_without_https = url_without_https.replace("www.", "")
     if url_without_https.endswith('/'):
         url_without_https = url_without_https[:-1]  # Remove the final slash
     return url_without_https
