9f08849fed9141643124d4c3de5921bf50342224
Andrew Le <a.asterisk.le@gmail.com>
2024-07-27 14:37:22 -0500

    Replaced select tags with datalist tags and deleted unncessary "other" functionality controlled by jquery. Applied to more jobs.

--- apptracker/static/js/applicationsubmission.js
+++ apptracker/static/js/applicationsubmission.js
@@ -4,22 +4,12 @@ $(document).ready(function() {
 
       var formData = new FormData(this); // Initialize FormData with the form data
 
-      // Loop through 'other_' fields and append them to formData object
-      $('[id^="other_"]').each(function() {
-          var fieldName = $(this).attr('id').replace('other_', '');
-          var selectField = document.getElementById(fieldName + '_id');
-          var selectedValue = selectField.options[selectField.selectedIndex].value;
-          if (selectedValue === "other") {
-              formData.append(fieldName, $(this).val());
-          }
-      });
-
       $.ajax({
         type: 'POST',
         url: '/application_submission/',
-        data: formData, // Use formData object
-        processData: false, // Prevent jQuery from automatically converting formDataObject to a query string
-        contentType: false, // Prevent jQuery from automatically setting Content-Type header
+        data: formData, 
+        processData: false, 
+        contentType: false, 
         beforeSend: function(xhr, settings) {
             xhr.setRequestHeader("X-CSRFToken", '{{ csrf_token }}');
         },
@@ -40,27 +30,3 @@ $(document).ready(function() {
   });
 });
 
-
-document.addEventListener("DOMContentLoaded", function() {
-  // Function to show/hide input field based on select option
-  function toggleInput(selectId, inputId) {
-    const select = document.getElementById(selectId);
-    const input = document.getElementById(inputId);
-    select.addEventListener("change", function() {
-      if (select.value === "other") {
-        input.classList.remove("d-none");
-        input.required = true; // Make input required
-      } else {
-        input.classList.add("d-none");
-        input.required = false; // Make input optional
-        input.value = ""; // Clear the input field
-      }
-    });
-  }
-
-  // Toggle input fields for employer and location
-  toggleInput("employer_id", "other_employer");
-  toggleInput("location_id", "other_location");
-  toggleInput("source_id", "other_source");
-
-  });
